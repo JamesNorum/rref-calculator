8fb71d7fdd79eb23c37e43c92d0d76bbb56d964d
Zach Stence <zachstence@gmail.com>
2018-09-07 20:20:21 -0500

    Add random matrix generator, improve output

--- rref-calculator.py
+++ rref-calculator.py
@@ -1,15 +1,21 @@
+from random import randint
+
 def displayMatrix(matrix):
   for row in matrix:
     print('[', end='')
     for cell in row:
-      print('{0:>6.2f}'.format(cell + 0), end=' ')
+      print('{0:>7.2f}'.format(cell + 0), end=' ')
     print(']')
   print()
 
-
-
-
-
+def randomMatrix(rows, cols):
+  m = []
+  for i in range(rows):
+    row = []
+    for i in range(cols):
+      row.append(randint(0, 10))
+    m.append(row)
+  return m
 
 def leadingOne(matrix, i):
   # divide every cell in that row by the number in the spot going to be the
@@ -28,8 +34,6 @@ def pivot(matrix, i):
       scale = -1 * row[i]
       matrix[r] = [curr + scale * other for curr, other in zip(matrix[r], matrix[i])]
 
-
-
 def rref(matrix):
   for i in range(len(matrix)):
     leadingOne(matrix, i)
@@ -39,25 +43,25 @@ def rref(matrix):
 
 
 
-m1 = [[1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9]]
+# m1 = [[1, 2, 3],
+#       [4, 5, 6],
+#       [7, 8, 9]]
 
-m2 = [[1, 2, 1],
-      [-2, -3, 1],
-      [3, 5, 0]]
+# m2 = [[1, 2, 1],
+#       [-2, -3, 1],
+#       [3, 5, 0]]
 
-m3 = [[-1, 1, -4, 2],
-      [0, 2, 5, -6],
-      [2, 3, 1, -2]]
+# m3 = [[-1, 1, -4, 2],
+#       [0, 2, 5, -6],
+#       [2, 3, 1, -2]]
 
-m4 = [[1, -1, -1, 2, 0],
-      [0, 1, 4, 3, 10],
-      [0, 0, 1, 1, 2,]]
+# m4 = [[1, -1, -1, 2, 0],
+#       [0, 1, 4, 3, 10],
+#       [0, 0, 1, 1, 2,]]
 
-# leadingOne(m2, 0)
-# pivot(m2, 0)
-# leadingOne(m2, 1)
-# pivot(m2, 1)
+m = randomMatrix(5, 8)
 
-displayMatrix(rref(m4))
+print('Original matrix:')
+displayMatrix(m)
+print('RREF form:')
+displayMatrix(rref(m))
